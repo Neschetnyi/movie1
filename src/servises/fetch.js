63f1d1556f709cae1af2c008a4c86b07561ff2e5
Neschetnyi <a.neschetnyi@gmail.com>
2025-02-03 17:12:36 +0500

    adding fetch service

---
+++ src/servises/fetch.js
@@ -0,0 +1,15 @@
+const url =
+  "https://api.themoviedb.org/3/search/movie?query=a&include_adult=false&language=en-US&page=1";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTc2OTJjZDJkMDc2MWMyMTBlM2U3OTEyYjM3ODVmZiIsIm5iZiI6MTczODU4MDQ4MC4xMzc5OTk4LCJzdWIiOiI2N2EwYTIwMGFjNWE3OTUxYjljYjg4YmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.6ps5tpaC0EvpAYP-JToYDZE5E_I5HlxSyhsi4OpM6tU",
+  },
+};
+
+fetch(url, options)
+  .then((res) => res.json())
+  .then((json) => console.log(json))
+  .catch((err) => console.error(err));
--- src/servises/getData.js
+++ src/servises/getData.js
@@ -1,15 +1,35 @@
 class GetData {
-  async getResources(url) {
-    const res = await fetch(url);
+  url = "https://api.themoviedb.org/3/search/";
+  options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTc2OTJjZDJkMDc2MWMyMTBlM2U3OTEyYjM3ODVmZiIsIm5iZiI6MTczODU4MDQ4MC4xMzc5OTk4LCJzdWIiOiI2N2EwYTIwMGFjNWE3OTUxYjljYjg4YmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.6ps5tpaC0EvpAYP-JToYDZE5E_I5HlxSyhsi4OpM6tU",
+    },
+  };
+
+  async getResources(url, options) {
+    const res = await fetch(url, options);
     await res.then((value) => {
       console.log(value.json());
     });
     return await res.json();
   }
 
-  getData() {
-    return this.getResources();
+  async getAllMovies() {
+    let allMovies = await this.getResources(
+      `${this.url}movie?query=a&include_adult=false&language=en-US&page=1`,
+      this.options
+    );
+    return allMovies;
   }
 }
+
+const getData = new GetData();
+
+getData.getAllMovies().then((value) => {
+  console.log(value);
+});
 
 export default GetData;
