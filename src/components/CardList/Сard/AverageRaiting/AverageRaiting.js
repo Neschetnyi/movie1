90460c0890ee36443c7b17ae4a738fdc2d0ef899
Neschetnyi <a.neschetnyi@gmail.com>
2025-02-12 02:28:02 +0500

    end of styling

--- src/components/CardList/Сard/AverageRaiting/AverageRaiting.js
+++ src/components/CardList/Сard/AverageRaiting/AverageRaiting.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
+import { Avatar } from "antd";
+import "./AverageRaiting.css";
 
 class AverageRaiting extends Component {
+  state = {
+    colors: ["#E90000", "#E97E00", "#E9D100", "#66E900"],
+  };
+
   render() {
+    console.log("AverageRaiting props", this.props);
+    let color = "";
+    if (this.props.vote_average >= 7) {
+      color = this.state.colors[3];
+    } else if (this.props.vote_average >= 5) {
+      color = this.state.colors[2];
+    } else if (this.props.vote_average >= 3) {
+      color = this.state.colors[1];
+    } else {
+      color = this.state.colors[0];
+    }
     return (
       <Avatar
         size={42}
         style={{
-          color: "#f56a00",
+          color: "black",
           backgroundColor: "transparent",
-          boxShadow: "inset 0 0 0 1px #f56a00",
+          boxShadow: `inset 0 0 0 2px ${color}`,
         }}
       >
-        {this.props.vote_average}
+        {Math.trunc(this.props.vote_average * 10) / 10}
       </Avatar>
     );
   }
 }
+
+export default AverageRaiting;
--- src/components/CardList/Сard/Сard.js
+++ src/components/CardList/Сard/Сard.js
@@ -3,10 +3,22 @@ import "./Card.css";
 import { Tag } from "antd";
 import cardBG from "./cardBG.png";
 import RateMovie from "./RateMovie/RateMovie";
-import { Avatar } from "antd";
+import AverageRaiting from "./AverageRaiting/AverageRaiting";
 
 function Card({ card }) {
-  console.log("single card", card);
+  console.log(
+    `single card ${card.title} card.title.lenght: ${card.title.length}`,
+    card
+  );
+
+  let heightTitleRegulation = () => {
+    if (card.title.length > 35) {
+      return "80px";
+    } else {
+      return null;
+    }
+  };
+
   let date = new Date();
 
   let cardPoster = `https://image.tmdb.org/t/p/w500${card.backdrop_path}`;
@@ -21,29 +33,25 @@ function Card({ card }) {
       </div>
       <div className="Content">
         <div>
-          <div>
-            <h2 className="Content_h2">{card.title}</h2>
+          <div className="ContentHeader">
+            <div className="ContentHeaderH2">
+              <h2 className="Content_h2">{card.title}</h2>
+            </div>
+            <div>
+              <AverageRaiting {...card} />
+            </div>
           </div>
-          <div>
-            <Avatar
-              size={42}
-              style={{
-                color: "#f56a00",
-                backgroundColor: "transparent",
-                boxShadow: "inset 0 0 0 1px #f56a00",
-              }}
-            >
-              {card.vote_average}
-            </Avatar>
-          </div>
-        </div>
 
-        <p className="Date">{date.toLocaleDateString()}</p>
-        <div className="Tags">
-          <Tag>Action</Tag>
-          <Tag>Drama</Tag>
+          <div className="Date">{date.toLocaleDateString()}</div>
+          <div className="Tags">
+            <Tag>Action</Tag>
+            <Tag>Drama</Tag>
+          </div>
+
+          <div className="Text" style={{ height: heightTitleRegulation() }}>
+            {card.overview}
+          </div>
         </div>
-        <p className="Text">{card.overview}</p>
         <div className="Rating">
           <RateMovie />
         </div>
